eb40b350df0ecdbc4d53b724ddafad8245e30e87
Max Shapira <max.shapira@icloud.com>
2024-06-18 14:43:05 +0300

    rename toString import to avoid shadowing global property

--- src/utils/remarkReadingTime.ts
+++ src/utils/remarkReadingTime.ts
@@ -1,10 +1,10 @@
-import { toString } from "mdast-util-to-string";
+import { toString as toStringUtil } from "mdast-util-to-string";
 import getReadingTime from "reading-time";
 
 export function remarkReadingTime() {
   // @ts-expect-error:next-line
   return (tree, { data }) => {
-    const textOnPage = toString(tree);
+    const textOnPage = toStringUtil(tree);
     const readingTime = getReadingTime(textOnPage);
     // readingTime.text will give us minutes read as a friendly string,
     // i.e. "3 min read"
