34493c2e6642458250f7ce044359533ad8306165
Max Shapira <max.shapira@icloud.com>
2024-06-21 16:26:16 +0300

    fix typo in site title

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
   // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
   author: "Max Shapira",
   // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-  title: "Max Shapira - Code and Life",
+  title: "Max Shapira - Life and Code",
   // Meta property used as the default description meta property
   description:
     "Early-bird Python developer who codes, exercises, and reads before wrangling two toddlers.",
