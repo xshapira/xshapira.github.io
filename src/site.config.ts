b0f28c2d3eb259c7097dbdecae94a3bcfe8d34ad
Max Shapira <max.shapira@icloud.com>
2024-06-30 15:43:44 +0300

    enable themed scrollbars for code blocks

--- src/site.config.ts
+++ src/site.config.ts
@@ -54,7 +54,7 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
     // return default selector
     return `[data-theme="${theme.name}"]`;
   },
-  useThemedScrollbars: false,
+  useThemedScrollbars: true,
   styleOverrides: {
     frames: {
       frameBoxShadowCssValue: "none",
